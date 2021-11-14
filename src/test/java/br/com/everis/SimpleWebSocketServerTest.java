1e04da4e8f3658c6e43ad6fcf2e27f024d0900b3
Rogerio Fontes <fontestz@gmail.com>
2021-11-13 21:29:45 -0300

    Initial commit

---
+++ src/test/java/br/com/everis/SimpleWebSocketServerTest.java
@@ -0,0 +1,59 @@
+package br.com.everis;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Duration;
+
+import io.micronaut.rxjava2.http.client.websockets.RxWebSocketClient;
+import jakarta.inject.Inject;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+
+@MicronautTest
+class SimpleWebSocketServerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleWebSocketServerTest.class);
+
+    @Inject
+    @Client("http://localhost:8186")
+    RxWebSocketClient client;
+
+    SimpleWebSocketClient webSocketClient;
+
+    @BeforeEach
+    void connect() {
+        webSocketClient = client.connect(
+                        SimpleWebSocketClient.class,
+                        "/ws/simple/prices")
+                .blockingFirst();
+        LOG.info("Client session: {}", webSocketClient.getSession());
+    }
+
+    @Test
+    void canReceiveMessagesWithClient() {
+        webSocketClient.send("Hello");
+        Awaitility.await().timeout(Duration.ofSeconds(10)).untilAsserted(() -> {
+            final Object[] messages = webSocketClient.getObservedMessages().toArray();
+            LOG.info("Observed Messages {} - {}", webSocketClient.getObservedMessages().size(), messages);
+            assertEquals("Connected!", messages[0]);
+            assertEquals("Not supported => (Hello)", messages[1]);
+        });
+    }
+
+    @Test
+    void canSendReactively() {
+        LOG.info("Sent {}", webSocketClient.sendReactive("Hello").blockingGet());
+        Awaitility.await().timeout(Duration.ofSeconds(10)).untilAsserted(() -> {
+            final Object[] messages = webSocketClient.getObservedMessages().toArray();
+            LOG.info("Observed Messages {} - {}", webSocketClient.getObservedMessages().size(), messages);
+            assertEquals("Connected!", messages[0]);
+            assertEquals("Not supported => (Hello)", messages[1]);
+        });
+    }
+}
